dd04bf6ad58cf6b565dfe0f0fba208c43fa5dbd4
Kai Henrichs <k.henrichs@tagwise.de>
2014-10-20 23:31:09 +0200

    play with variables, replace() and append()

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,7 @@
-$("#main").append("tagwise UG")
+$("#main").append("tagwise UG");
+
+var awesomeThought = "I am legend and I am awesome!";
+console.log(awesomeThought);
+
+var funThought = awesomeThought.replace("awesome", "fun");
+$("#main").append(funThought);
